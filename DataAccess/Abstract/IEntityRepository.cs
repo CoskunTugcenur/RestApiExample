b9b9bf91494e4ace9ef28d1a458777b0bfc66cf3
TugcenurC <coskuntugcenur@gmail.com>
2021-02-06 02:48:23 +0300

    EF added

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<T>
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        
+
+    }
+}
